d1980a777e3425a31f9ea21d2221276f99047c87
Cristian Duque <techahead2@gmail.com>
2023-05-15 15:31:51 -0400

    added bulk and strength page functions for front and back ends

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const videos = require("./routes/videos");
+const video = require("./routes/vid");
 const cors = require("cors");
 
 // This middleware allows us to post JSON in request.body
 app.use(cors());
 app.use(express.json());
 
-app.use("/videos", express.static("./public/images"));
-
 app.use("/videos", videos);
+app.use("/vid", video);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
